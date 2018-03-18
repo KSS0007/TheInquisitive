19797335d25cb37ad13d9c0110e7151585378c68
Emily Mahoney <emilymahoneyis@gmail.com>
2018-03-18 17:21:35 -0400

    The main page and maintenance page are now linked together. Pressing the maintenance button will launch the maintenance page window.

--- Proj3/src/MainPage.java
+++ Proj3/src/MainPage.java
@@ -12,12 +12,15 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.TitledBorder;
 import java.awt.SystemColor;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.Button;
-
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MainPage 
 {
@@ -62,7 +65,6 @@ private void initialize()
  		frmSearchEngine.getContentPane().add(panel5);
  		panel5.setLayout(null);
  		
- 		
 		JLabel lblSearchEngine = new JLabel("Search Engine");
 		lblSearchEngine.setBounds(250, 0, 140, 30);
 		lblSearchEngine.setHorizontalAlignment(SwingConstants.CENTER);
@@ -79,32 +81,85 @@ private void initialize()
 		txtSearchHere.setBounds(100, 37, 300, 16);
 		txtSearchHere.setHorizontalAlignment(SwingConstants.LEFT);
 		txtSearchHere.setFont(new Font("SansSerif", Font.PLAIN, 11));
-		panel4.add(txtSearchHere);
+		panel4.add(txtSearchHere);	
+		
+        // Grouping the RadioButtons together.
+		ButtonGroup bgroup = new ButtonGroup();
 		
 		JRadioButton jRadioButton1 = new JRadioButton("All of these Search Terms");
 		jRadioButton1.setBounds(40, 70, 180, 27);
 		jRadioButton1.setFont(new Font("SansSerif", Font.PLAIN, 13));
 		panel4.add(jRadioButton1);
+		bgroup.add(jRadioButton1);
 		
 		JRadioButton jRadioButton2 = new JRadioButton("Any of these Search Terms");
 		jRadioButton2.setBounds(250, 70, 200, 27);
 		jRadioButton2.setFont(new Font("SansSerif", Font.PLAIN, 13));
 		panel4.add(jRadioButton2);
+		bgroup.add(jRadioButton2);
 		
 		JRadioButton jRadioButton3 = new JRadioButton("Exact Phrase");
 		jRadioButton3.setBounds(450, 70, 180, 27);
 		jRadioButton3.setFont(new Font("SansSerif", Font.PLAIN, 13));
 		panel4.add(jRadioButton3);
-		
+		bgroup.add(jRadioButton3);
+
 		JButton btnsearch = new JButton("Search");
-		btnsearch.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		btnsearch.setBounds(500, 35, 90, 20);
 		btnsearch.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		panel4.add(btnsearch);
+		panel4.add(btnsearch);		
+		
+		if (jRadioButton1.isSelected()) {
+			//When RadioButtion is selected, the searching happens here.
+			//To search all search terms.
+			String word = "";
+			String fileNames = "";
+			String files;
+			String inputWord = null;
+			File folder = new File("Test.txt");
+			File[] listOfFiles = folder.listFiles();
+			File file = new File("Test.txt");
+			
+			try {
+				//Searches the file for all words within the search text.
+				Scanner input = new Scanner(file);
+				while (input.hasNext()) {
+					word = input.next();
+					if (inputWord.equals(word)) {
+						//Adds the file names into a list.
+						for (int i= 0; i < listOfFiles.length; i++) {
+						if (listOfFiles[i].isFile()) {
+							files = listOfFiles[i].getName();
+							fileNames += "\n" + file;
+						}
 
+						//lblSearchTerms.setText(fileNames);
+						}
+					}
+				}
+			}catch (Exception error) {
+				
+			}
+			//Scanner s1 = new Scanner(System.in);
+			//String folderPath = s1.next();
+			//File folder = new File(folderPath);
+			//File[] listOfFiles = folder.listFiles();
+			
+			//if (folder.isDirectory()) {
+
+				//if (listOfFiles.length > 1) {
+					//lblSearchTerms.setText("No matching files found.");
+				//}else {
+					
+				//}
+			//}
+			//To search any search terms.
+			} else if (jRadioButton2.isSelected()) {
+			
+        } else if (jRadioButton3.isSelected()) {
+        	
+        }
+	    
 		JLabel lblNumberIndex = new JLabel("Number of files indexed:  0");
 		lblNumberIndex.setBounds(245, 0, 200,60);
 		lblNumberIndex.setHorizontalAlignment(SwingConstants.LEFT);
@@ -112,18 +167,18 @@ private void initialize()
 		panel5.add(lblNumberIndex);
 		
 		JButton btnmaintenance = new JButton("Maintenance");
-		btnmaintenance.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				frame1 nw = new frame1();
-				nw.NewScreen();
-				
-			}
-		});
 		btnmaintenance.setBounds(20, 20, 119, 20);
 		btnmaintenance.setFont(new Font("SansSerif", Font.PLAIN, 15));
 		panel5.add(btnmaintenance);
 		frmSearchEngine.getContentPane().add(panel5);
+		
+		btnmaintenance.addActionListener(new ActionListener() { 
+			public void actionPerformed (ActionEvent arg0){
+
+				frame1 nw = new frame1();
+				nw.NewScreen();
+		}
+		});
 		
 		JButton btnabout = new JButton("About");
 		btnabout.setBounds(530, 20, 90, 20);
@@ -132,6 +187,5 @@ private void initialize()
 		frmSearchEngine.getContentPane().add(panel5);
 
 		
-
 }
 }
--- Proj3/src/frame1.java
+++ Proj3/src/frame1.java
@@ -21,178 +21,150 @@ import java.awt.event.ActionEvent;
 
 public class frame1 {
 
-	private JFrame frmSearchEngineMaintenance;
-	private JPanel panel_2;
-	private JPanel panel;
-	private JLabel lblSearchEngine;
-	private JPanel panel_1;
-	private JLabel lblNewLabel;
-	private JPanel panel_3;
-	private JLabel lblSearchEngineVersion;
+		private JFrame frmSearchEngineMaintenance;
+		private JPanel panel_2;
+		private JPanel panel;
+		private JLabel lblSearchEngine;
+		private JPanel panel_1;
+		private JLabel lblNewLabel;
+		private JPanel panel_3;
+		private JLabel lblSearchEngineVersion;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void NewScreen() {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					frame1 window = new frame1();
-					window.frmSearchEngineMaintenance.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+		/**
+		 * Launch the application.
+		 */
+		public static void NewScreen() {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						frame1 window = new frame1();
+						window.frmSearchEngineMaintenance.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+
+		/**
+		 * Create the application.
+		 */
+		public frame1() {
+			initialize();
+		}
+
+		/**
+		 * Initialize the contents of the frame.
+		 */
+		private void initialize() {
+			frmSearchEngineMaintenance = new JFrame();
+			frmSearchEngineMaintenance.setType(Type.POPUP);
+			frmSearchEngineMaintenance.setTitle("Search Engine Maintenance");
+			frmSearchEngineMaintenance.getContentPane().setBackground(Color.WHITE);
+			frmSearchEngineMaintenance.setBounds(100, 100, 667, 541);
+			frmSearchEngineMaintenance.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frmSearchEngineMaintenance.getContentPane().setLayout(null);
+			
+			panel_2 = new JPanel();
+			panel_2.setBounds(317, 64, 332, 39);
+			frmSearchEngineMaintenance.getContentPane().add(panel_2);
+			panel_2.setLayout(null);
+			
+			JLabel lblStatus = new JLabel("Status");
+			lblStatus.setBounds(0, 0, 331, 39);
+			panel_2.add(lblStatus);
+			lblStatus.setHorizontalAlignment(SwingConstants.CENTER);
+			lblStatus.setFont(new Font("SansSerif", Font.PLAIN, 15));
+			
+			panel = new JPanel();
+			panel.setBounds(0, 64, 318, 39);
+			frmSearchEngineMaintenance.getContentPane().add(panel);
+			panel.setLayout(null);
+			
+			JLabel lblFileName = new JLabel("File Name");
+			lblFileName.setBounds(0, 0, 318, 39);
+			panel.add(lblFileName);
+			lblFileName.setFont(new Font("SansSerif", Font.PLAIN, 15));
+			lblFileName.setHorizontalAlignment(SwingConstants.CENTER);
+			
+			panel_1 = new JPanel();
+			panel_1.setBackground(SystemColor.activeCaptionBorder);
+			panel_1.setBounds(0, 0, 649, 64);
+			frmSearchEngineMaintenance.getContentPane().add(panel_1);
+			panel_1.setLayout(null);
+			
+			lblSearchEngine = new JLabel("Search Engine - Index Maintence");
+			lblSearchEngine.setBounds(0, 0, 649, 64);
+			panel_1.add(lblSearchEngine);
+			lblSearchEngine.setFont(new Font("SansSerif", Font.BOLD, 18));
+			lblSearchEngine.setHorizontalAlignment(SwingConstants.CENTER);
+			
+			panel_3 = new JPanel();
+			panel_3.setBackground(SystemColor.scrollbar);
+			panel_3.setBounds(0, 413, 649, 81);
+			frmSearchEngineMaintenance.getContentPane().add(panel_3);
+			panel_3.setLayout(null);
+			
+			lblSearchEngineVersion = new JLabel("Search Engine Version 1.0");
+			lblSearchEngineVersion.setBounds(503, 65, 146, 16);
+			panel_3.add(lblSearchEngineVersion);
+			lblSearchEngineVersion.setHorizontalAlignment(SwingConstants.RIGHT);
+			lblSearchEngineVersion.setFont(new Font("SansSerif", Font.PLAIN, 12));
+			
+			lblNewLabel = new JLabel("");
+			lblNewLabel.setBounds(0, 413, 649, 81);
+			frmSearchEngineMaintenance.getContentPane().add(lblNewLabel);
+			
+			JButton addFile = new JButton("Add File");
+			addFile.setBounds(90, 2, 80, 30);
+
+			JButton rebuildOutOfDate = new JButton("Rebuild Out-of-Date");
+			rebuildOutOfDate.setBounds(250, 2, 150, 30);
+			
+			JButton removeSelectedFile = new JButton("Remove Selected Files");
+			removeSelectedFile.setBounds(450, 2, 170, 30);
+			
+			JButton resetWindows = new JButton("Reset Windows");
+			resetWindows.setBounds(10, 49, 140, 30);
+			
+			addFile.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null, 
+							"This button does not work");
 				}
 			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public frame1() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmSearchEngineMaintenance = new JFrame();
-		frmSearchEngineMaintenance.setType(Type.POPUP);
-		frmSearchEngineMaintenance.setTitle("Search Engine Maintenance");
-		frmSearchEngineMaintenance.getContentPane().setBackground(Color.WHITE);
-		frmSearchEngineMaintenance.setBounds(100, 100, 667, 541);
-		frmSearchEngineMaintenance.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmSearchEngineMaintenance.getContentPane().setLayout(null);
-		
-		panel_2 = new JPanel();
-		panel_2.setBounds(317, 64, 332, 39);
-		frmSearchEngineMaintenance.getContentPane().add(panel_2);
-		panel_2.setLayout(null);
-		
-		JLabel lblStatus = new JLabel("Status");
-		lblStatus.setBounds(0, 0, 331, 39);
-		panel_2.add(lblStatus);
-		lblStatus.setHorizontalAlignment(SwingConstants.CENTER);
-		lblStatus.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		
-		panel = new JPanel();
-		panel.setBounds(0, 64, 318, 39);
-		frmSearchEngineMaintenance.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblFileName = new JLabel("File Name");
-		lblFileName.setBounds(0, 0, 318, 39);
-		panel.add(lblFileName);
-		lblFileName.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		lblFileName.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		panel_1 = new JPanel();
-		panel_1.setBackground(SystemColor.activeCaptionBorder);
-		panel_1.setBounds(0, 0, 649, 64);
-		frmSearchEngineMaintenance.getContentPane().add(panel_1);
-		panel_1.setLayout(null);
-		
-		lblSearchEngine = new JLabel("Search Engine - Index Maintence");
-		lblSearchEngine.setBounds(0, 0, 649, 64);
-		panel_1.add(lblSearchEngine);
-		lblSearchEngine.setFont(new Font("SansSerif", Font.BOLD, 18));
-		lblSearchEngine.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		panel_3 = new JPanel();
-		panel_3.setBackground(SystemColor.scrollbar);
-		panel_3.setBounds(0, 413, 649, 81);
-		frmSearchEngineMaintenance.getContentPane().add(panel_3);
-		panel_3.setLayout(null);
-		
-		lblSearchEngineVersion = new JLabel("Search Engine Version 1.0");
-		lblSearchEngineVersion.setBounds(503, 65, 146, 16);
-		panel_3.add(lblSearchEngineVersion);
-		lblSearchEngineVersion.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblSearchEngineVersion.setFont(new Font("SansSerif", Font.PLAIN, 12));
-		
-		lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(0, 413, 649, 81);
-		frmSearchEngineMaintenance.getContentPane().add(lblNewLabel);
-		
-		JButton addFile = new JButton("Add File");
-		addFile.setBounds(90, 2, 80, 30);
-
-		JButton rebuildOutOfDate = new JButton("Rebuild Out-of-Date");
-		rebuildOutOfDate.setBounds(250, 2, 150, 30);
-		
-		JButton removeSelectedFile = new JButton("Remove Selected Files");
-		removeSelectedFile.setBounds(450, 2, 170, 30);
-		
-		JButton resetWindows = new JButton("Reset Windows");
-		resetWindows.setBounds(10, 49, 140, 30);
-		
-		addFile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				String fileName = "letters.txt";
-				String line = null;
-				
-	            try {
-	                // FileReader reads text files in the default encoding.
-	                FileReader fileReader = 
-	                    new FileReader(fileName);
-
-	                // Always wrap FileReader in BufferedReader.
-	                BufferedReader bufferedReader = 
-	                    new BufferedReader(fileReader);
-
-	                while((line = bufferedReader.readLine()) != null) {
-	                    System.out.println(line);
-	                }   
-
-	                // Always close files.
-	                bufferedReader.close();         
-	            }
-	            catch(FileNotFoundException ex) {
-	                System.out.println(
-	                    "Unable to open file '" + 
-	                    fileName + "'");                
-	            }
-	            catch(IOException ex) {
-	                System.out.println(
-	                    "Error reading file '" 
-	                    + fileName + "'");                  
-	            }
-	        }
-		}
-
-				);
-		rebuildOutOfDate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, 
-						"This button does not work");
+					);
+			rebuildOutOfDate.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null, 
+							"This button does not work");
+				}
 			}
-		}
-				);
-		removeSelectedFile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, 
-						"This button does not work");
+					);
+			removeSelectedFile.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null, 
+							"This button does not work");
+				}
 			}
-		}
-				);
-		resetWindows.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, 
-						"This button does not work");
+					);
+			resetWindows.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null, 
+							"This button does not work");
+				}
 			}
+					);
+			
+			panel_3.add(addFile);
+			frmSearchEngineMaintenance.getContentPane().add(panel_3);
+			panel_3.add(rebuildOutOfDate);
+			frmSearchEngineMaintenance.getContentPane().add(panel_3);
+			panel_3.add(removeSelectedFile);
+			frmSearchEngineMaintenance.getContentPane().add(panel_3);
+			panel_3.add(resetWindows);
+			frmSearchEngineMaintenance.getContentPane().add(panel_3);
+			
 		}
-				);
-		
-		panel_3.add(addFile);
-		frmSearchEngineMaintenance.getContentPane().add(panel_3);
-		panel_3.add(rebuildOutOfDate);
-		frmSearchEngineMaintenance.getContentPane().add(panel_3);
-		panel_3.add(removeSelectedFile);
-		frmSearchEngineMaintenance.getContentPane().add(panel_3);
-		panel_3.add(resetWindows);
-		frmSearchEngineMaintenance.getContentPane().add(panel_3);
-		
-	}
+	
 }
